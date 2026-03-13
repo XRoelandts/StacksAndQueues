c19ed0a681b69fa2a49595aef5fd4d11dbfc2c8b
xavierroelandts <xkroelandts@mail.com>
2026-03-12 20:43:45 -0500

    Initial commit

---
+++ src/Decode.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+public class Decode {
+    public static void main(String[] args) {
+        System.out.println(decodeString("3[a]2[bc]"));
+        System.out.println(decodeString("3[a2[c]]"));
+        System.out.println(decodeString("2[abc]3[cd]ef"));
+    }
+
+    public static String decodeString(String s) {
+
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> stringStack = new Stack<>();
+
+        String currentString = "";
+        int k = 0;
+
+        for (char ch : s.toCharArray()) {
+
+            // If digit → build full number (handles multi-digit like 12[a])
+            if (Character.isDigit(ch)) {
+                k = k * 10 + (ch - '0');
+            }
+
+            // Opening bracket → push state
+            else if (ch == '[') {
+                countStack.push(k);
+                stringStack.push(currentString);
+
+                currentString = "";
+                k = 0;
+            }
+            // Closing bracket → decode
+            else if (ch == ']') {
+                int repeat = countStack.pop();
+                String previous = stringStack.pop();
+
+                StringBuilder temp = new StringBuilder(previous);
+
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(currentString);
+                }
+
+                currentString = temp.toString();
+            }
+
+            // Letter → build current substring
+            else {
+                currentString += ch;
+            }
+        }
+
+        return currentString;
+    }
+
+}
